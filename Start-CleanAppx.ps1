b3b183ce715ee5a12353008f58d0d1f779d19e63
Rene B <judoka_rene@hotmail.com>
2019-02-28 19:16:52 +0100

    Update Start-CleanAppx.ps1

--- Start-CleanAppx.ps1
+++ Start-CleanAppx.ps1
@@ -1,61 +1,21 @@
+## Writes a verbose output to the screen for user information
+    Write-Host "Retriving current disk percent free for comparison once the script has completed.                 " -NoNewline -ForegroundColor Green
+    Write-Host "[DONE]" -ForegroundColor Green -BackgroundColor Black
+
+    ## Gathers the amount of disk space used before running the script
+    $Before = Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq "3" } | Select-Object SystemName,
+    @{ Name = "Drive" ; Expression = { ( $_.DeviceID ) } },
+    @{ Name = "Size (GB)" ; Expression = {"{0:N1}" -f ( $_.Size / 1gb)}},
+    @{ Name = "FreeSpace (GB)" ; Expression = {"{0:N1}" -f ( $_.Freespace / 1gb ) } },
+    @{ Name = "PercentFree" ; Expression = {"{0:P1}" -f ( $_.FreeSpace / $_.Size ) } } |
+        Format-Table -AutoSize |
+        Out-String
+
+
 ## start cleaning the appxPackages
     Function Start-CleanAppx{
      Write-Host "Windows AppxPackage Cleanup is running.                                                                  " -NoNewline -ForegroundColor Green
     Try{
-<#    
-Get-AppxPackage *Microsoft.3dbuilder* | Remove-AppxPackage
-Get-AppxPackage *AdobeSystemsIncorporated.AdobePhotoshopExpress* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.WindowsAlarms* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.Asphalt8Airborne* | Remove-AppxPackage
-Get-AppxPackage *microsoft.windowscommunicationsapps* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.WindowsCamera* | Remove-AppxPackage
-Get-AppxPackage *king.com.CandyCrushSodaSaga* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.DrawboardPDF* | Remove-AppxPackage
-Get-AppxPackage *Facebook* | Remove-AppxPackage
-Get-AppxPackage *BethesdaSoftworks.FalloutShelter* | Remove-AppxPackage
-Get-AppxPackage *FarmVille2CountryEscape* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.WindowsFeedbackHub* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.GetHelp* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.Getstarted* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.ZuneMusic* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.WindowsMaps* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.Messaging* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.Wallet* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.MicrosoftSolitaireCollection* | Remove-AppxPackage
-Get-AppxPackage *Todos* | Remove-AppxPackage
-Get-AppxPackage *ConnectivityStore* | Remove-AppxPackage
-Get-AppxPackage *MinecraftUWP* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.OneConnect* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.BingFinance* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.ZuneVideo* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.BingNews* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage
-Get-AppxPackage *Netflix* | Remove-AppxPackage
-Get-AppxPackage *OneNote* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.MSPaint* | Remove-AppxPackage
-Get-AppxPackage *PandoraMediaInc* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.People* | Remove-AppxPackage
-Get-AppxPackage *CommsPhone* | Remove-AppxPackage
-Get-AppxPackage *windowsphone* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.Print3D* | Remove-AppxPackage
-Get-AppxPackage *flaregamesGmbH.RoyalRevolt2* | Remove-AppxPackage
-Get-AppxPackage *WindowsScan* | Remove-AppxPackage
-Get-AppxPackage *AutodeskSketchBook* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.SkypeApp* | Remove-AppxPackage
-Get-AppxPackage *bingsports* | Remove-AppxPackage
-Get-AppxPackage *Office.Sway* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.Getstarted* | Remove-AppxPackage
-Get-AppxPackage *Twitter* | Remove-AppxPackage
-Get-AppxPackage *Microsoft3DViewer* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.WindowsSoundRecorder* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.BingWeather* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.XboxApp* | Remove-AppxPackage
-Get-AppxPackage *XboxOneSmartGlass* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.XboxSpeechToTextOverlay* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.XboxIdentityProvider* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.XboxGameOverlay* | Remove-AppxPackage
-#>
-
 
 # New method that should be implemented
 $AppList = @(
@@ -123,3 +83,21 @@ foreach ($App in $AppList) {
   }
 
 } Start-CleanAppx 
+
+## Stop timer
+    $Enders = (Get-Date)
+
+    ## Calculate amount of seconds your code takes to complete.
+    Write-Verbose "Elapsed Time: $(($Enders - $Starters).totalseconds) seconds
+ ## Sends hostname to the console for ticketing purposes.
+    Write-Host (Hostname) -ForegroundColor Green
+
+    ## Sends the date and time to the console for ticketing purposes.
+    Write-Host (Get-Date | Select-Object -ExpandProperty DateTime) -ForegroundColor Green
+
+    ## Sends the disk usage before running the cleanup script to the console for ticketing purposes.
+    Write-Verbose "
+    Before: $Before"
+
+    ## Sends the disk usage after running the cleanup script to the console for ticketing purposes.
+    Write-Verbose "After: $After"
