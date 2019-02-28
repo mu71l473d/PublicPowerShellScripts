f06e13a587fceaddff0606854d398f80f7084232
Rene B <judoka_rene@hotmail.com>
2019-02-28 19:39:37 +0100

    Update Start-CleanAppx.ps1

--- Start-CleanAppx.ps1
+++ Start-CleanAppx.ps1
@@ -1,25 +1,8 @@
-## Writes a verbose output to the screen for user information
-    Write-Host "Retriving current disk percent free for comparison once the script has completed.                 " -NoNewline -ForegroundColor Green
-    Write-Host "[DONE]" -ForegroundColor Green -BackgroundColor Black
-
-    ## Gathers the amount of disk space used before running the script
-    $Before = Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq "3" } | Select-Object SystemName,
-    @{ Name = "Drive" ; Expression = { ( $_.DeviceID ) } },
-    @{ Name = "Size (GB)" ; Expression = {"{0:N1}" -f ( $_.Size / 1gb)}},
-    @{ Name = "FreeSpace (GB)" ; Expression = {"{0:N1}" -f ( $_.Freespace / 1gb ) } },
-    @{ Name = "PercentFree" ; Expression = {"{0:P1}" -f ( $_.FreeSpace / $_.Size ) } } |
-        Format-Table -AutoSize |
-        Out-String
-
-
 ## start cleaning the appxPackages
     Function Start-CleanAppx{
-     Write-Host "Windows AppxPackage Cleanup is running.                                                                  " -NoNewline -ForegroundColor Green
-    Try{
+     Write-Host "Windows AppxPackage Cleanup is running." -NoNewline -ForegroundColor Green
 
-# New method that should be implemented
-$AppList = @(
-"*Microsoft.3dbuilder* | Remove-AppxPackage",
+"*Microsoft.3dbuilder*",
 "*AdobeSystemsIncorporated.AdobePhotoshopExpress*",
 "*Microsoft.WindowsAlarms*",
 "*Microsoft.Asphalt8Airborne*",
@@ -69,35 +52,6 @@ $AppList = @(
 "*XboxOneSmartGlass*",
 "*Microsoft.XboxSpeechToTextOverlay*",
 "*Microsoft.XboxIdentityProvider*",
-"*Microsoft.XboxGameOverlay*"
-)
-
-foreach ($App in $AppList) {
-    Get-AppxPackage -Name $App | Remove-AppxPackage -allusers -ErrorAction SilentlyContinue
-}
-
-
-  } Catch [System.Exception]{
-      Write-host "Some apps could not be uninstalled" -ForegroundColor Red -NoNewline
-      Write-host "[ERROR]" -ForegroundColor Red -BackgroundColor black
-  }
+"*Microsoft.XboxGameOverlay*" | foreach {Get-AppxPackage $_ | Remove-AppxPackage}
 
 } Start-CleanAppx 
-
-## Stop timer
-    $Enders = (Get-Date)
-
-    ## Calculate amount of seconds your code takes to complete.
-    Write-Verbose "Elapsed Time: $(($Enders - $Starters).totalseconds) seconds
- ## Sends hostname to the console for ticketing purposes.
-    Write-Host (Hostname) -ForegroundColor Green
-
-    ## Sends the date and time to the console for ticketing purposes.
-    Write-Host (Get-Date | Select-Object -ExpandProperty DateTime) -ForegroundColor Green
-
-    ## Sends the disk usage before running the cleanup script to the console for ticketing purposes.
-    Write-Verbose "
-    Before: $Before"
-
-    ## Sends the disk usage after running the cleanup script to the console for ticketing purposes.
-    Write-Verbose "After: $After"
