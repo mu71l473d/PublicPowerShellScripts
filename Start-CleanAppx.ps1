417996cb0345f1fb0749d3fe23cff16c01faf42f
Rene B <judoka_rene@hotmail.com>
2019-04-08 12:41:23 +0200

    Preparation for timer and disk usage delta

--- Start-CleanAppx.ps1
+++ Start-CleanAppx.ps1
@@ -1,8 +1,31 @@
+## Begin the timer
+$Starters = (Get-Date)
+
+## Check $VerbosePreference variable, and turns -Verbose on
+Function global:Write-Verbose ( [string]$Message ) {
+if ( $VerbosePreference -ne 'SilentlyContinue' ) {
+    Write-Host "$Message" -ForegroundColor 'Green'
+  }
+}
+
+
+ ## Writes a verbose output to the screen for user information
+    Write-Host "Retriving current disk percent free for comparison once the script has completed.                 " -NoNewline -ForegroundColor Green
+    Write-Host "[DONE]" -ForegroundColor Green -BackgroundColor Black
+
+## Gathers the amount of disk space used before running the script
+   $Before = Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq "3" } | Select-Object SystemName,
+   @{ Name = "Drive" ; Expression = { ( $_.DeviceID ) } },
+   @{ Name = "Size (GB)" ; Expression = {"{0:N1}" -f ( $_.Size / 1gb)}},
+   @{ Name = "FreeSpace (GB)" ; Expression = {"{0:N1}" -f ( $_.Freespace / 1gb ) } },
+   @{ Name = "PercentFree" ; Expression = {"{0:P1}" -f ( $_.FreeSpace / $_.Size ) } } | Format-Table -AutoSize | Out-String
+
 ## start cleaning the appxPackages
     Function Start-CleanAppx{
-     Write-Host "Windows AppxPackage Cleanup is running." -NoNewline -ForegroundColor Green
 
-"*Microsoft.3dbuilder*",
+Write-Host "Windows AppxPackage Cleanup is running." -NoNewline -ForegroundColor Green
+
+$AppList = @("*Microsoft.3dbuilder*",
 "*AdobeSystemsIncorporated.AdobePhotoshopExpress*",
 "*Microsoft.WindowsAlarms*",
 "*Microsoft.Asphalt8Airborne*",
@@ -57,10 +80,32 @@
 "*SpotifyMusic*", 
 "*Disney*",
 "*BubbleWitch3Saga*",
-"*MarchofEmpires*"| foreach {Get-AppxPackage -allusers $_ | Remove-AppxPackage }
+"*MarchofEmpires*") | foreach {Get-AppxPackage -allusers $_ | Remove-AppxPackage }
 
-} Start-CleanAppx 
+}Start-CleanAppx 
 
+## gathers disk usage after running the cleanup cmdlets.
+$After = Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq "3" } | Select-Object SystemName,
+@{ Name = "Drive" ; Expression = { ( $_.DeviceID ) } },
+@{ Name = "Size (GB)" ; Expression = {"{0:N1}" -f ( $_.Size / 1gb)}},
+@{ Name = "FreeSpace (GB)" ; Expression = {"{0:N1}" -f ( $_.Freespace / 1gb ) } },
+@{ Name = "PercentFree" ; Expression = {"{0:P1}" -f ( $_.FreeSpace / $_.Size ) } } |
+Format-Table -AutoSize | Out-String
 
-#    Get-AppxPackage | Select Name  # for finding new packages to deleter
-#"**"         for copy paste new lines
+## Stop timer
+$Enders = (Get-Date)
+
+## Calculate amount of seconds your code takes to complete.
+Write-Verbose "Elapsed Time: $(($Enders - $Starters).totalseconds) seconds"
+
+## Sends hostname to the console for ticketing purposes.
+Write-Host (Hostname) -ForegroundColor Green
+
+## Sends the date and time to the console for ticketing purposes.
+Write-Host (Get-Date | Select-Object -ExpandProperty DateTime) -ForegroundColor Green
+
+## Sends the disk usage before running the cleanup script to the console for ticketing purposes.
+Write-Verbose "Before: $Before"
+
+## Sends the disk usage after running the cleanup script to the console for ticketing purposes.
+Write-Verbose "After: $After"
