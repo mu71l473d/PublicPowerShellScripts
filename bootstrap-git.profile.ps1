a7f6664db880d950f3beb162c70e22faf42b3ecc
mu71l473d <judoka_rene@hotmail.com>
2019-06-23 00:45:53 +0200

    new version

---
+++ bootstrap-git.profile.ps1
@@ -0,0 +1,16 @@
+﻿# Start a transcript
+#
+if (!(Test-Path "$Env:USERPROFILE\Documents\WindowsPowerShell\Transcripts"))
+{
+    if (!(Test-Path "$Env:USERPROFILE\Documents\WindowsPowerShell"))
+    {
+        $rc = New-Item -Path "$Env:USERPROFILE\Documents\WindowsPowerShell" -ItemType directory
+    }
+    $rc = New-Item -Path "$Env:USERPROFILE\Documents\WindowsPowerShell\Transcripts" -ItemType directory
+}
+$curdate = $(get-date -Format "yyyyMMddhhmmss")
+Start-Transcript -Path "$Env:USERPROFILE\Documents\WindowsPowerShell\Transcripts\PowerShell_transcript.$curdate.txt"
+
+# Alias Git
+#
+New-Alias -Name git -Value "$Env:ProgramFiles\Git\bin\git.exe"
