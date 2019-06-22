22b2ddf2b3f863b2de23f5888af6fd89312421cf
mu71l473d <judoka_rene@hotmail.com>
2019-06-23 01:11:45 +0200

    new version

---
+++ UploadToGithub.ps1
@@ -0,0 +1,78 @@
+﻿echo "BashScripts"
+cd ~/Github/BashScripts
+git pull
+git add *
+git commit -m "new version"
+git push
+
+echo "PublicBashScripts"
+cd ~/Github/PublicBashScripts
+git pull
+git add *
+git commit -m "new version"
+git push
+
+echo "BanditChallenge"
+cd ~/Github/BanditChallenge
+git pull
+git add *
+git commit -m "new version"
+git push
+
+echo "GreyBlackHatPython"
+cd ~/Github/GreyBlackHatPython
+git pull
+git add *
+git commit -m "new version"
+git push
+
+echo "hacking-taoe"
+cd ~/Github/hacking-taoe
+git pull
+git add *
+git commit -m "new version"
+git push
+
+echo "JavaProjects"
+cd ~/Github/JavaProjects
+git pull
+git add *
+git commit -m "new version"
+git push
+
+echo "PowershellScripts"
+cd ~/Github/Powershellscripts
+git pull
+git add *
+git commit -m "new version"
+git push
+
+echo "PublicPythonScripts"
+cd ~/Github/PublicPythonScripts
+git pull
+git add *
+git commit -m "new version"
+git push
+
+echo "PythonScripts"
+cd ~/Github/PythonScripts
+git pull
+git add *
+git commit -m "new version"
+git push
+
+echo "Books and booknotes"
+git pull
+cd D:/books/
+git add *
+git push
+
+echo "PowershellScripts"
+cd ~/Github/PublicPowershellScripts
+git pull
+git add *
+git commit -m "new version"
+git push
+
+
+echo "Done with Uploading"
---
+++ gitclone.ps1
@@ -0,0 +1,24 @@
+﻿git config --global user.name "mu71l473d"
+git config --global user.email "judoka_rene@hotmail.com"
+
+git clone https://github.com/mu71l473d/PublicBashScripts.git;
+git clone https://github.com/mu71l473d/BashScripts.git;
+git clone https://github.com/mu71l473d/BookNotes.git; 
+git clone https://github.com/mu71l473d/GreyBlackHatPython.git; 
+git clone https://github.com/mu71l473d/PythonScripts.git;
+git clone https://github.com/mu71l473d/PowershellScripts.git;
+git clone https://github.com/mu71l473d/BanditChallenge.git;
+git clone https://github.com/mu71l473d/JavaProjects.git;
+git clone https://github.com/mu71l473d/hacking-taoe.git;
+git clone https://github.com/mu71l473d/PublicPythonScripts.git
+git clone https://github.com/mu71l473d/mu71l473d.github.io.git
+git clone https://github.com/mu71l473d/PublicPowerShellScripts.git
+git clone https://github.com/mu71l473d/MagicMirror.git
+git clone https://github.com/mu71l473d/CProgrammingLanguage.git
+git clone https://github.com/mu71l473d/PublicPythonScripts.git;
+git clone https://github.com/mu71l473d/mu71l473d.github.io.git;
+git clone https://github.com/mu71l473d/PublicPowerShellScripts.git;
+git clone https://github.com/mu71l473d/MagicMirror.git;
+git clone https://github.com/mu71l473d/CProgrammingLanguage.git;
+git clone https://github.com/dahlbyk/posh-git.git
+
